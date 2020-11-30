1820582c73fba36b258667fe772b340dbda6f142
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2020-12-01 01:45:45 +0200

    Changed method openFile to getWordsFromFile Added a temporary option in the menu to test regex results

--- ProphecyFinder/src/prophecyfinder/Tools.java
+++ ProphecyFinder/src/prophecyfinder/Tools.java
@@ -3,22 +3,38 @@ package prophecyfinder;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 
 public class Tools {
 
-    /* Method that reads the file. Return type has been set temporarily to boolean
-    until we decide a data type
+    /* Method that reads the specified file and returns a HashSet with all 
+       the words in it. Hashset has been chosen, as there is no need of
+       multiple word occurences 
      */
-    public static boolean openFile(String fileName) {
-        BufferedReader textFile;
+    public static HashSet getWordsFromFile(String fileName) {
+
+        HashSet<String> wordsInFile = new HashSet();
+        String line;
+        String regex = "[.,?!;:_\\- ]+";//TODO needs to be refined
+        String[] words;
+
         try {
-            textFile = new BufferedReader(new FileReader(fileName));
-            System.out.println("File has been found!");
+            BufferedReader textFile = new BufferedReader(new FileReader(fileName));
+            while ((line = textFile.readLine()) != null) {
+                words = line.split(regex);
+                for (String i : words) {
+                    if (!i.equals("")) {
+                        wordsInFile.add(i.toUpperCase());
+                    }
+                }
+            }
             textFile.close();
+            return wordsInFile;
         } catch (IOException ex) {
             System.out.println("File not found!");
-            System.exit(0+0);
+            System.exit(0);
         }
-        return true;
+
+        return wordsInFile;
     }
 }
